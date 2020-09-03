2e8a1e154682123db9cd574d141d5af86780c958
Jiayi-Joyce-Lu <jllu1@student.unimelb.edu.au>
2020-09-03 18:29:40 +0800

    nothing changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,40 +17,40 @@ export default function Header() {
   }
 
   return (
-    <div className="addAuthor">
-      Add New Author
-      <form>
-        <input
-          type="text"
-          placeholder="Author id"
-          name="id"
-          // value={id}
-          // onChange={event => {
-          //   setId(event.target.value);
-          // }}
-        />
-        <input
-          type="text"
-          placeholder="First Name"
-          name="first_name"
-          // value={first_name}
-          // onChange={event => {
-          //   setFirstName(event.target.value);
-          // }}
-        />
-        <input
-          type="text"
-          placeholder="Last Name"
-          name="last_name"
-          // value={last_name}
-          // onChange={event => {
-          //   setLastName(event.target.value);
-          // }}
-        />
-        <Button className={"btn-success"} onClick={onSubmit}>
-          Save
-        </Button>
-      </form>
-    </div>
+      <div className="addAuthor">
+        Add New Author
+        <form>
+          <input
+              type="text"
+              placeholder="Author id"
+              name="id"
+              // value={id}
+              // onChange={event => {
+              //   setId(event.target.value);
+              // }}
+          />
+          <input
+              type="text"
+              placeholder="First Name"
+              name="first_name"
+              // value={first_name}
+              // onChange={event => {
+              //   setFirstName(event.target.value);
+              // }}
+          />
+          <input
+              type="text"
+              placeholder="Last Name"
+              name="last_name"
+              // value={last_name}
+              // onChange={event => {
+              //   setLastName(event.target.value);
+              // }}
+          />
+          <Button className={"btn-success"} onClick={onSubmit}>
+            Save
+          </Button>
+        </form>
+      </div>
   );
 }
