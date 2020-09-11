8bc776232967f8a67c851251648808dad0279c42
zhiqingw <zhiqingw@student.unimelb.edu.au>
2020-09-11 21:55:55 +0800

    navigation icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import HomeIcon from "@material-ui/icons/Home";
+import IconButton from "@material-ui/core/IconButton";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import PortfolioIcon from "@material-ui/icons/Collections";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItem from "@material-ui/core/ListItem";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 export default function Nav() {
     return (
         <nav>
+            <NavLink exact to="/">
+                <HomeIcon />
+            </NavLink>
 
-        <NavLink exact to="/">
-            Home
-        </NavLink>
         <NavLink exact to="/blog">
-            Design Square
+            <PortfolioIcon />
         </NavLink>
         <NavLink exact to="/usercenter">
-            User Center
+            <AccountCircleIcon />
         </NavLink>
     </nav>
 )
+}
+
+function backHomePage(){
+    window.location.assign(`http://localhost:3000/`);
 }
