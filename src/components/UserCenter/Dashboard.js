913818956572b2fd5e3dbea5be21e6f4e9160b30
Jiayi-Joyce-Lu <jllu1@student.unimelb.edu.au>
2020-09-12 14:00:22 +0800

    Update Dashboard.js

--- src/components/UserCenter/Dashboard.js
+++ src/components/UserCenter/Dashboard.js
@@ -227,7 +227,7 @@ export default function Dashboard() {
                         {/* Recent Deposits */}
                         <Grid item xs={12} md={4} lg={3}>
                             <Paper className={fixedHeightPaper}>
-                                <Deposits />
+
                             </Paper>
                         </Grid>
                         {/* Recent Orders */}
