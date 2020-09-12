8f59c73f5849c85f5cfd16a72cb8eea29e8bb044
Picket <z9@student.unimelb.edu.au>
2020-09-12 22:00:33 +0800

    Updated the FileUpdate component
    Updated the FileUpdate component to make the file upload only after clicking the update button.

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,10 +1,13 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import ReactDom from 'react-dom';
 import ReactS3, {uploadFile} from 'react-s3';
 import S3FileUpload from 'react-s3';
 import BackupIcon from '@material-ui/icons/Backup';
+import '../views/FileUpload.css'
+import UploadButton from "./Button/UploadButton";
 
 export default function FileUpload () {
+
     const config = {
         bucketName: 'itproject',
         dirName: 'file', /* optional */
@@ -12,21 +15,29 @@ export default function FileUpload () {
         accessKeyId: 'AKIAJ6JITOOOA3AF5QWA',
         secretAccessKey: '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',
     };
-    const upload = async e => {
-        console.log(e.target.files[0]);
-        uploadFile(e.target.files[0], config)
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    function onClickHandler() {
+        console.log(selectedFile);
+        uploadFile(selectedFile, config)
             .then(data => console.log(data))
             .catch(err => console.error(err))
-    };
-
+    }
 
     return(
-        <div className="file">
-            <h1> upload</h1>
-            <p><BackupIcon /></p>
-            <input type='file' onChange={upload}/>
-
-
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <form method="post" action="#" id="#">
+                        <div className="form-group files color">
+                            <label>Upload Your File </label>
+                            <input type="file" className="form-control" multiple="" type="file" name="file" onChange={event => setSelectedFile(event.target.files[0])} />
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <button type="button" className="btn btn-success btn-block" onClick={onClickHandler}>Upload</button>
         </div>
     );
 };
