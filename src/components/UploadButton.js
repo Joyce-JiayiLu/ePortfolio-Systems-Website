95cf62bf75fa68940eb5ae920863de6b3463f10a
Picket <z9@student.unimelb.edu.au>
2020-09-12 13:57:48 +0800

    UploadFile Component
    Finished the uploadfile component and uploadbutton component and removed unnecessary imports in Home.js.

--- src/components/UploadButton.js
+++ src/components/UploadButton.js
@@ -2,6 +2,7 @@ import React from "react";
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import Button from '@material-ui/core/Button';
 import {makeStyles} from "@material-ui/core/styles";
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
     button: {
@@ -13,12 +14,22 @@ export default function SaveButton({ className, children, onClick, ...props }) {
 
     const classes = useStyles();
 
+    function onClickHandler() {
+        const data = new FormData();
+        data.append('file', this.state.selectedFile);
+        axios.post("http://localhost:3000/upload", data, {
+        }).then(res => {
+            console.log(res.statusText)
+        })
+    }
+
     return (
         <Button
             variant="contained"
             color="default"
             className={classes.button}
             startIcon={<CloudUploadIcon />}
+            onClick={onClickHandler}
         >
             Upload
         </Button>
