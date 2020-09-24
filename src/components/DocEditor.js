636c3207c02590b295c42f12ef2e7f03f6b1887a
Picket <z9@student.unimelb.edu.au>
2020-09-24 21:07:27 +0800

    Added preview for document editor
    Added preview funtion for document editor, now contents can be previewed in a new pop-up window.

--- src/components/DocEditor.js
+++ src/components/DocEditor.js
@@ -5,7 +5,7 @@ import BraftEditor from 'braft-editor'
 export default class DocEditor extends React.Component {
 
     state = {
-        editorState: BraftEditor.createEditorState('<p>Hello <b>World!</b></p>'), // 设置编辑器初始内容
+        editorState: BraftEditor.createEditorState('<p>Hello <b>World!</b></p>'), // set initial contents for the editor
         outputHTML: '<p></p>'
     }
 
@@ -32,9 +32,88 @@ export default class DocEditor extends React.Component {
         })
     }
 
+    preview = () => {
+
+        if (window.previewWindow) {
+            window.previewWindow.close()
+        }
+
+        window.previewWindow = window.open()
+        window.previewWindow.document.write(this.buildPreviewHtml())
+        window.previewWindow.document.close()
+
+    }
+
+    buildPreviewHtml () {
+
+        return `
+      <!Doctype html>
+      <html>
+        <head>
+          <title>Preview Content</title>
+          <style>
+            html,body{
+              height: 100%;
+              margin: 0;
+              padding: 0;
+              overflow: auto;
+              background-color: #f1f2f3;
+            }
+            .container{
+              box-sizing: border-box;
+              width: 1000px;
+              max-width: 100%;
+              min-height: 100%;
+              margin: 0 auto;
+              padding: 30px 20px;
+              overflow: hidden;
+              background-color: #fff;
+              border-right: solid 1px #eee;
+              border-left: solid 1px #eee;
+            }
+            .container img,
+            .container audio,
+            .container video{
+              max-width: 100%;
+              height: auto;
+            }
+            .container p{
+              white-space: pre-wrap;
+              min-height: 1em;
+            }
+            .container pre{
+              padding: 15px;
+              background-color: #f1f1f1;
+              border-radius: 5px;
+            }
+            .container blockquote{
+              margin: 0;
+              padding: 15px;
+              background-color: #f1f1f1;
+              border-left: 3px solid #d1d1d1;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">${this.state.editorState.toHTML()}</div>
+        </body>
+      </html>
+    `
+
+    }
+
     render () {
 
         const { editorState, outputHTML } = this.state
+
+        const extendControls = [
+            {
+                key: 'custom-button',
+                type: 'button',
+                text: '预览',
+                onClick: this.preview
+            }
+        ]
 
         return (
             <div>
@@ -42,6 +121,8 @@ export default class DocEditor extends React.Component {
                     <BraftEditor
                         value={editorState}
                         onChange={this.handleChange}
+                        extendControls={extendControls}
+                        contentStyle={{height: 400}}
                     />
                 </div>
                 {/*<h5>输出内容</h5>*/}
