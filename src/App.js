dc88e8b9a553e0f8c81d6491cd9987711d4759f2
ElliotXue <61861224+ElliotXue@users.noreply.github.com>
2020-09-24 21:51:40 +1000

    dynamic homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{Component} from "react";
+import React, {Component, Suspense, useMemo, useRef} from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Nav from "./components/Nav";
@@ -14,7 +14,114 @@ import Users from "./pages/Users";
 import PortfolioCollections from "./components/UserCenter/PortfolioCollections";
 import Resume from "./components/UserCenter/Resume";
 import Callback from "./pages/Callback";
+import {Canvas, useFrame, useLoader, useThree} from "react-three-fiber";
+import {LinearFilter, Object3D, TextureLoader, WebGLRenderTarget} from "three";
+import textureUrl from "./assets/233.jpg";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import diamondUrl from "./assets/diamond.glb";
+import BackfaceMaterial from "./backface-material";
+import RefractionMaterial from "./refraction-material";
+import "./views/Homepage.css"
 
+
+function Background() {
+    const { viewport, aspect } = useThree()
+    const texture = useLoader(TextureLoader, textureUrl)
+    useMemo(() => (texture.minFilter = LinearFilter), [])
+    // Calculates a plane filling the screen similar to background-size: cover
+    const adaptedHeight = 3800 * (aspect > 5000 / 3800 ? viewport.width / 5000 : viewport.height / 3800)
+    const adaptedWidth = 5000 * (aspect > 5000 / 3800 ? viewport.width / 5000 : viewport.height / 3800)
+    return (
+        <mesh layers={1} scale={[adaptedWidth, adaptedHeight, 1]}>
+            <planeBufferGeometry attach="geometry" />
+            <meshBasicMaterial attach="material" map={texture} depthTest={false} />
+        </mesh>
+    )
+}
+
+function Diamonds() {
+    const { size, viewport, gl, scene, camera, clock } = useThree()
+    const model = useRef()
+    const gltf = useLoader(GLTFLoader, diamondUrl)
+
+    // Create Fbo's and materials
+    const [envFbo, backfaceFbo, backfaceMaterial, refractionMaterial] = useMemo(() => {
+        const envFbo = new WebGLRenderTarget(size.width, size.height)
+        const backfaceFbo = new WebGLRenderTarget(size.width, size.height)
+        const backfaceMaterial = new BackfaceMaterial()
+        const refractionMaterial = new RefractionMaterial({ envMap: envFbo.texture, backfaceMap: backfaceFbo.texture, resolution: [size.width, size.height] })
+        return [envFbo, backfaceFbo, backfaceMaterial, refractionMaterial]
+    }, [size])
+
+    // Create random position data
+    const dummy = useMemo(() => new Object3D(), [])
+    const diamonds = useMemo(
+        () =>
+            new Array(80).fill().map((_, i) => ({
+                position: [i < 5 ? 0 : viewport.width / 2 - Math.random() * viewport.width, 40 - Math.random() * 40, i < 5 ? 26 : 10 - Math.random() * 20],
+                factor: 0.1 + Math.random(),
+                direction: Math.random() < 0.5 ? -1 : 1,
+                rotation: [Math.sin(Math.random()) * Math.PI, Math.sin(Math.random()) * Math.PI, Math.cos(Math.random()) * Math.PI]
+            })),
+        []
+    )
+
+    // Render-loop
+    useFrame(() => {
+        // Update instanced diamonds
+        diamonds.forEach((data, i) => {
+            const t = clock.getElapsedTime()
+            data.position[1] -= (data.factor / 5) * data.direction
+            if (data.direction === 1 ? data.position[1] < -50 : data.position[1] > 50)
+                data.position = [i < 5 ? 0 : viewport.width / 2 - Math.random() * viewport.width, 50 * data.direction, data.position[2]]
+            const { position, rotation, factor } = data
+            dummy.position.set(position[0], position[1], position[2])
+            dummy.rotation.set(rotation[0] + t * factor, rotation[1] + t * factor, rotation[2] + t * factor)
+            dummy.scale.set(1 + factor, 1 + factor, 1 + factor)
+            dummy.updateMatrix()
+            model.current.setMatrixAt(i, dummy.matrix)
+        })
+        model.current.instanceMatrix.needsUpdate = true
+        // Render env to fbo
+        gl.autoClear = false
+        camera.layers.set(1)
+        gl.setRenderTarget(envFbo)
+        gl.render(scene, camera)
+        // Render cube backfaces to fbo
+        camera.layers.set(0)
+        model.current.material = backfaceMaterial
+        gl.setRenderTarget(backfaceFbo)
+        gl.clearDepth()
+        gl.render(scene, camera)
+        // Render env to screen
+        camera.layers.set(1)
+        gl.setRenderTarget(null)
+        gl.render(scene, camera)
+        gl.clearDepth()
+        // Render cube with refraction material to screen
+        camera.layers.set(0)
+        model.current.material = refractionMaterial
+        gl.render(scene, camera)
+    }, 1)
+
+    return (
+        <instancedMesh ref={model} args={[null, null, diamonds.length]}>
+            <bufferGeometry dispose={false} attach="geometry" {...gltf.__$[1].geometry} />
+            <meshBasicMaterial attach="material" />
+        </instancedMesh>
+    )
+}
+
+export function Ap() {
+    return (
+        <Canvas camera={{ fov: 50, position: [0, 0, 30] }}>
+            <Suspense fallback={null}>
+                <Background />
+                <Diamonds />
+            </Suspense>
+        </Canvas>
+    )
+}
 
 
 export default class App extends Component {
@@ -28,7 +135,6 @@ export default class App extends Component {
                     <Switch>
                         <Route exact path="/">
                             <Nav/>
-                            <Home/>
                         </Route>
                         <Route path="/login">
                             <Nav/>
@@ -58,7 +164,7 @@ export default class App extends Component {
                         </Route>
                     </Switch>
                 </div>
-
+                <Ap/>
             </Router>
 
         );
--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Auth from "./Auth";
 import {Auth0Provider} from '@auth0/auth0-react';
+import { TextureLoader, WebGLRenderTarget, Object3D, LinearFilter } from "three"
+import { Suspense, useMemo, useRef } from "react"
+import { Canvas, useLoader, useThree, useFrame } from "react-three-fiber"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import BackfaceMaterial from "./backface-material"
+import RefractionMaterial from "./refraction-material"
+import diamondUrl from "./assets/diamond.glb"
+import textureUrl from "./assets/233.jpg"
+import "./views/Homepage.css"
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const auth = new Auth();
-
 const rootElement = document.getElementById("root");
 let state = {};
+
+
+
+
+
 window.setState = (changes) => {
     state = Object.assign({},state,changes);
     ReactDOM.render(<App {...state}/>, rootElement);
