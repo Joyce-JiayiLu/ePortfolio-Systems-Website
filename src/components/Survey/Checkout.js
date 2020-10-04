f08e93b7cc273395a2f65ed8c0bb2e2af4237dc3
Picket <z9@student.unimelb.edu.au>
2020-10-04 14:07:41 +0800

    Text change
    Submission success information modification.

--- src/components/Survey/Checkout.js
+++ src/components/Survey/Checkout.js
@@ -151,11 +151,10 @@ export default function Checkout() {
                         {activeStep === steps.length ? (
                             <React.Fragment>
                                 <Typography variant="h5" gutterBottom>
-                                    Thank you for your order.
+                                    Congratulations, your submission is successful.
                                 </Typography>
                                 <Typography variant="subtitle1">
-                                    Your order number is #2001539. We have emailed your order confirmation, and will
-                                    send you an update when your order has shipped.
+                                    Your portfolio shall be displayed in Design Square soon.
                                 </Typography>
                             </React.Fragment>
                         ) : (
